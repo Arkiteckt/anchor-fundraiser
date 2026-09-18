2022349aed9652f93db4855a7e5af5999fa9ff28
Your Name <your.email@example.com>
2026-09-18 13:39:38 -0400

    feat: NFT receipt on first contribution

--- programs/fundraiser/src/lib.rs
+++ programs/fundraiser/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7WferfAMCt6f32DYucuQNhnSYdoV7SWSR92od8t1jDzW");
+declare_id!("3C3G49g4HJqxncfBVFfNu4NkWfdKzjoKgewNJC5DkTM4");
 
 mod state;
 mod instructions;
